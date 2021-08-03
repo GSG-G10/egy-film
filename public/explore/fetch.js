cf961df587d88e60ad0aff0012239244e51715ab
ahmadfarid <sabbahahmad093@gmail.com>
2021-08-03 22:32:42 +0300

    rename api.js to fetch.js and move it to explore folder

---
+++ public/explore/fetch.js
@@ -0,0 +1,14 @@
+const fetchData = (url, cb) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                cb(JSON.parse(xhr.responseText));
+            } else {
+                cb(xhr.status);
+            }
+        }
+    };
+    xhr.open("GET", url);
+    xhr.send();
+}
