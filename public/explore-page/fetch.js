0cd597c0d10fcccb8673c672c4990992655e9745
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-04 12:43:34 +0300

    fetching explore API and append data to the main Relates #24

--- public/explore-page/fetch.js
+++ public/explore-page/fetch.js
@@ -1,15 +1,21 @@
+
 const fetchData = (url, cb) => {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        cb(JSON.parse(xhr.responseText));
-      } else {
-        cb(xhr.status);
-      }
-    }
-  };
-  xhr.open('GET', url);
-  xhr.send();
-};
-module.exports = fetchData;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(JSON.parse(xhr.responseText))
+                cb(JSON.parse(xhr.responseText));
+            } else {
+                cb(xhr.status);
+            }
+        }
+    };
+    xhr.open("GET", url);
+    xhr.send();
+}
+// module.exports = fetchData
+const apiKey = process.env.API_KEY;
+const searchQuery = "";
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`
+fetchData(url,showResult)
