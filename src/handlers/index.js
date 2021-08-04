e4c38378d74ffb9b1f4e8c522e48e2e5b739016f
umar <umars.salah@gmail.com>
2021-08-04 16:11:26 +0300

    Style the autoComplete box Relates #39

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -3,4 +3,6 @@ const errorHandler = require('./Error');
 const publicHandler = require('./Public');
 const moviesNamesHandler = require('./moviesNames');
 
-module.exports = { homeHandler, errorHandler, publicHandler, moviesNamesHandler };
+module.exports = {
+  homeHandler, errorHandler, publicHandler, moviesNamesHandler,
+};
